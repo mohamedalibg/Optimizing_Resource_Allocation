4d7652ac2f393d1e5f4ae27e365a7badbe149056
mohamedalibg <mohamedalibengharbia.est@gmail.com>
2024-03-06 17:23:57 +0100

    Front start

--- users/views.py
+++ users/views.py
@@ -61,3 +61,16 @@ class UserView(APIView):
 
 
         return Response(serializer.data)
+    
+
+class LogoutView(APIView):
+    def post(self, request):
+        response=Response()
+        response.delete_cookie('jwt')
+        response.data={
+            'message':'success'
+
+        }
+        return response
+
+
